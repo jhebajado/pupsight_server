c3fc2eb8269f57dc14feafbd98494fb7951cbc41
im-neiru <cantorne.neil@gmail.com>
2024-03-06 17:23:25 +0800

    feat: setup server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_multipart::Multipart;
+use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use dotenvy::dotenv;
+
+async fn process_image(_payload: Multipart) -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().content_type("application/json").finish())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let server_url = std::env::var("SERVER_URL").expect("SERVER_URL must be set");
+
+    println!("SERVER_URL: {server_url}");
+
+    HttpServer::new(|| App::new().route("/scan", web::post().to(process_image)))
+        .bind(server_url)?
+        .run()
+        .await
 }
