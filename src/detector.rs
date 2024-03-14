dd4d9946e72398cf03f610d59f5b4baf3b95f18d
im-neiru <cantorne.neil@gmail.com>
2024-03-15 05:57:08 +0800

    feat: add detector

---
+++ src/detector.rs
@@ -0,0 +1,203 @@
+use image::{DynamicImage, GenericImageView};
+use ndarray::{s, Array, Axis};
+use ort::{tensor::InputTensor, Environment, ExecutionProvider, InMemorySession};
+use tokio::sync::Mutex;
+
+pub(crate) struct Detector {
+    session: Mutex<InMemorySession<'static>>,
+}
+
+impl Detector {
+    const MAX_BYTES_RECIP: f32 = 1.0 / 255.0;
+
+    pub(crate) fn new() -> Self {
+        let environment = Environment::builder()
+            .with_log_level(ort::LoggingLevel::Verbose)
+            .with_execution_providers([ExecutionProvider::cuda(), ExecutionProvider::onednn()])
+            .build()
+            .unwrap()
+            .into_arc();
+
+        let session = Mutex::new(
+            ort::SessionBuilder::new(&environment)
+                .unwrap()
+                .with_optimization_level(ort::GraphOptimizationLevel::Level3)
+                .unwrap()
+                .with_intra_threads(1)
+                .unwrap()
+                .with_model_from_memory(include_bytes!("../model.onnx"))
+                .unwrap(),
+        );
+
+        Self { session }
+    }
+
+    pub(crate) async fn infer(&self, image: &DynamicImage) -> Vec<ResultBox> {
+        let mut input = Array::zeros((1, 3, 640, 640)).into_dyn();
+
+        for pixel in image.pixels() {
+            let x = pixel.0 as _;
+            let y = pixel.1 as _;
+            let [r, g, b, _] = pixel.2 .0;
+            input[[0, 0, y, x]] = (r as f32) * Self::MAX_BYTES_RECIP;
+            input[[0, 1, y, x]] = (g as f32) * Self::MAX_BYTES_RECIP;
+            input[[0, 2, y, x]] = (b as f32) * Self::MAX_BYTES_RECIP;
+        }
+
+        let output = {
+            let model = self.session.lock().await;
+
+            model
+                .run([InputTensor::FloatTensor(input)])
+                .unwrap()
+                .first()
+                .unwrap()
+                .try_extract::<f32>()
+                .unwrap()
+                .view()
+                .t()
+                .to_owned()
+        };
+
+        let mut boxes = Vec::new();
+        let output = output.slice(s![.., .., 0]);
+        for row in output.axis_iter(Axis(0)) {
+            let row: Vec<_> = row.iter().copied().collect();
+            let (class_id, probability) = row
+                .iter()
+                .skip(4)
+                .enumerate()
+                .map(|(index, value)| (index, *value))
+                .reduce(|a, row| if row.1 > a.1 { row } else { a })
+                .unwrap();
+
+            if probability < 0.3 {
+                continue;
+            }
+
+            boxes.push(OutputBox::new(
+                row[0],
+                row[1],
+                row[2],
+                row[3],
+                probability,
+                Classification::from(class_id),
+            ));
+        }
+
+        boxes.sort_by(|box1, box2| box2.probability.total_cmp(&box1.probability));
+        let mut result = Vec::<ResultBox>::new();
+        while !boxes.is_empty() {
+            let first = boxes[0];
+            result.push(first.into_result_box());
+            boxes.retain(|box1| Self::iou(&first, box1) < 0.75)
+        }
+
+        result
+    }
+
+    #[inline(always)]
+    fn iou(a: &OutputBox, b: &OutputBox) -> f32 {
+        let intersection = {
+            let intersect_left = a.left.max(b.left);
+            let intersect_top = a.top.max(b.top);
+            let intersect_right = a.right.min(b.right);
+            let intersect_bottom = a.bottom.min(b.bottom);
+
+            let intersect_width = intersect_right - intersect_left;
+            let intersect_height = intersect_bottom - intersect_top;
+
+            intersect_width * intersect_height
+        };
+
+        let union = {
+            let box1_area = a.width * a.height;
+            let box2_area = b.width * b.height;
+
+            box1_area + box2_area - intersection
+        };
+
+        intersection / union
+    }
+}
+
+#[derive(Clone, Copy, Debug, serde::Serialize)]
+pub enum Classification {
+    Normal,
+    Incipient,
+}
+
+impl From<usize> for Classification {
+    fn from(value: usize) -> Self {
+        match value {
+            0 => Self::Normal,
+            1 => Self::Incipient,
+            _ => panic!("Invalid numerical value for classification"),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, serde::Serialize)]
+pub struct ResultBox {
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
+    probability: f32,
+    classification: Classification,
+}
+
+#[derive(Clone, Copy)]
+pub struct OutputBox {
+    left: f32,
+    top: f32,
+    right: f32,
+    bottom: f32,
+    width: f32,
+    height: f32,
+    probability: f32,
+    classification: Classification,
+}
+
+impl OutputBox {
+    #[inline(always)]
+    fn new(
+        x_center: f32,
+        y_center: f32,
+        width: f32,
+        height: f32,
+        probability: f32,
+        classification: Classification,
+    ) -> Self {
+        let w_half = width * 0.5;
+        let h_half = height * 0.5;
+
+        let left = x_center - w_half;
+        let top = y_center - h_half;
+        let right = x_center + w_half;
+        let bottom = y_center + h_half;
+
+        Self {
+            left,
+            top,
+            right,
+            bottom,
+            width,
+            height,
+            probability,
+            classification,
+        }
+    }
+
+    #[inline(always)]
+    fn into_result_box(self) -> ResultBox {
+        ResultBox {
+            x: self.left,
+            y: self.top,
+            width: self.width,
+            height: self.height,
+            probability: self.probability,
+            classification: self.classification,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::Mutex;
+mod detector;
 
 use actix_multipart::{Field, Multipart};
 use actix_web::{
@@ -8,38 +8,22 @@ use actix_web::{
 use dotenvy::dotenv;
 use futures::{StreamExt, TryStreamExt};
 use image::{imageops::FilterType, GenericImageView};
-use ndarray::{s, Array, Axis};
-use ort::{tensor::InputTensor, ExecutionProvider, InMemorySession, Session};
+
+use detector::Detector;
 
 fn main() -> std::io::Result<()> {
     dotenv().ok();
 
-    let environment = ort::Environment::builder()
-        .with_log_level(ort::LoggingLevel::Verbose)
-        .with_execution_providers([ExecutionProvider::cuda()])
-        .build()
-        .unwrap()
-        .into_arc();
-
-    let session = ort::SessionBuilder::new(&environment)
-        .unwrap()
-        .with_optimization_level(ort::GraphOptimizationLevel::Level3)
-        .unwrap()
-        .with_intra_threads(1)
-        .unwrap()
-        .with_model_from_memory(include_bytes!("../model.onnx"))
-        .unwrap();
-
-    let session_data = Data::new(Mutex::new(session));
+    let detector = Data::new(detector::Detector::new());
 
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
         .unwrap()
-        .block_on(start(session_data))
+        .block_on(start(detector))
 }
 
-async fn start(session: Data<Mutex<InMemorySession<'static>>>) -> std::io::Result<()> {
+async fn start(session: Data<Detector>) -> std::io::Result<()> {
     let server_url = std::env::var("SERVER_URL").expect("SERVER_URL must be set");
 
     println!("SERVER_URL: {server_url}");
@@ -52,7 +36,7 @@ async fn start(session: Data<Mutex<InMemorySession<'static>>>) -> std::io::Resul
 
 #[actix_web::post("/scan")]
 async fn process_image(
-    (session, mut payload): (web::Data<Mutex<InMemorySession<'static>>>, Multipart),
+    (detector, mut payload): (web::Data<Detector>, Multipart),
 ) -> Result<HttpResponse, Error> {
     println!("Proccessing image");
     if let Ok(Some(mut field)) = payload.try_next().await {
@@ -69,78 +53,9 @@ async fn process_image(
                 .resize_exact(640, 640, FilterType::CatmullRom)
         };
 
-        let mut input = Array::zeros((1, 3, 640, 640)).into_dyn();
-        for pixel in image.pixels() {
-            let x = pixel.0 as _;
-            let y = pixel.1 as _;
-            let [r, g, b, _] = pixel.2 .0;
-            input[[0, 0, y, x]] = (r as f32) / 255.0;
-            input[[0, 1, y, x]] = (g as f32) / 255.0;
-            input[[0, 2, y, x]] = (b as f32) / 255.0;
-        }
+        let result = detector.infer(&image).await;
 
-        println!("Inference start");
-
-        let output = {
-            let model = session.lock().unwrap();
-
-            model
-                .run([InputTensor::FloatTensor(input)])
-                .unwrap()
-                .first()
-                .unwrap()
-                .try_extract::<f32>()
-                .unwrap()
-                .view()
-                .t()
-                .to_owned()
-        };
-
-        println!("Inference success");
-        let mut boxes = Vec::new();
-        let output = output.slice(s![.., .., 0]);
-        for row in output.axis_iter(Axis(0)) {
-            let row: Vec<_> = row.iter().copied().collect();
-            let (class_id, probability) = row
-                .iter()
-                .skip(4)
-                .enumerate()
-                .map(|(index, value)| (index, *value))
-                .reduce(|a, row| if row.1 > a.1 { row } else { a })
-                .unwrap();
-            if probability < 0.3 {
-                continue;
-            }
-            let classification = Classification::from(class_id);
-            let xc = row[0];
-            let yc = row[1];
-            let w = row[2];
-            let h = row[3];
-            let x1 = xc - w / 2.0;
-            let x2 = xc + w / 2.0;
-            let y1 = yc - h / 2.0;
-            let y2 = yc + h / 2.0;
-            boxes.push(OutputBox {
-                x1,
-                y1,
-                x2,
-                y2,
-                classification,
-                probability,
-            });
-        }
-
-        boxes.sort_by(|box1, box2| box2.probability.total_cmp(&box1.probability));
-        let mut result = Vec::new();
-        while !boxes.is_empty() {
-            let first = boxes[0];
-            result.push(first);
-            boxes.retain(|box1| iou(&first, box1) < 0.75)
-        }
-
-        return Ok(HttpResponse::Ok()
-            .content_type("application/json")
-            .json(result));
+        return Ok(HttpResponse::Ok().json(result));
     }
 
     Ok(HttpResponse::NotAcceptable().finish())
@@ -155,52 +70,4 @@ pub async fn get_field_filedata(field: &mut Field) -> Result<Vec<u8>, Error> {
     }
 
     Ok(buffer)
-}
-
-#[derive(Clone, Copy, Debug, serde::Serialize)]
-pub enum Classification {
-    Normal,
-    Incipient,
-    Mature,
-    Hypermature,
-}
-
-#[derive(Clone, Copy, Debug, serde::Serialize)]
-pub struct OutputBox {
-    x1: f32,
-    y1: f32,
-    x2: f32,
-    y2: f32,
-    probability: f32,
-    classification: Classification,
-}
-
-impl From<usize> for Classification {
-    fn from(value: usize) -> Self {
-        match value {
-            0 => Self::Hypermature,
-            1 => Self::Incipient,
-            2 => Self::Mature,
-            3 => Self::Normal,
-            _ => panic!("Invalid numerical value for classification"),
-        }
-    }
-}
-
-fn iou(box1: &OutputBox, box2: &OutputBox) -> f32 {
-    intersection(box1, box2) / union(box1, box2)
-}
-
-fn union(box1: &OutputBox, box2: &OutputBox) -> f32 {
-    let box1_area = (box1.x2 - box1.x1) * (box1.y2 - box1.y1);
-    let box2_area = (box2.x2 - box2.x1) * (box2.y2 - box2.y1);
-    box1_area + box2_area - intersection(box1, box2)
-}
-
-fn intersection(box1: &OutputBox, box2: &OutputBox) -> f32 {
-    let x1 = box1.x1.max(box2.x1);
-    let y1 = box1.y1.max(box2.y1);
-    let x2 = box1.x2.min(box2.x2);
-    let y2 = box1.y2.min(box2.y2);
-    (x2 - x1) * (y2 - y1)
 }
